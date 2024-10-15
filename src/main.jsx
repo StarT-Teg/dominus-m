06dbeaad4e2a393f0ef3a5a4a742c77f2dda7877
StarT_T <romik.13@mail.ru>
2024-10-15 12:02:28 +0300

    deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
