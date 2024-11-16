3f8e33bf1a7bde20eae2ce46df3bdf69ef6c98c4
StarT_T <romik.13@mail.ru>
2024-11-16 22:55:08 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ const SongLine = forwardRef(({songName, position, rating}, ref) => {
     const songAvatarUrl = `${import.meta.env.BASE_URL}/${songName.toLowerCase().replaceAll(' ', '_')}.webp`
 
     return (<div className={'songLine'} ref={ref}>
-            <div className={'songLinePosition'}>{position}</div>
-            <div className={'songLineName'}>
-                <img src={songAvatarUrl} className={'songLineAvatar'} loading={'lazy'} onError={(e) => {
-                    e.target.onError = null;
-                    e.target.src = `${import.meta.env.BASE_URL}/vite.svg`
-                }} alt=""/>
-                {songName}
-            </div>
-            <div className={'songLineRating'}>{rating}</div>
-        </div>)
+        <div className={'songLinePosition'}>{position}</div>
+        <div className={'songLineName'}>
+            <img src={songAvatarUrl} className={'songLineAvatar'} loading={'lazy'} onError={(e) => {
+                e.target.onError = null;
+                e.target.src = `${import.meta.env.BASE_URL}/vite.svg`
+            }} alt=""/>
+            {songName}
+        </div>
+        <div className={'songLineRating'}>{rating}</div>
+    </div>)
 
 });
 
@@ -30,69 +30,79 @@ const getSongNameFromText = (text) => {
 }
 
 function App() {
-    const [offset, setOffset] = useState(0)
-    const {data, refetch} = useGetData(offset);
 
+    const MOCK_RATING = {
+        'Ancient Machines': [4, 6, 6, 8, 6.43, 7, 8, 7, 8, 6, 8, 6, 7, 6],
+        'City Colors': [8, 5.55, 8, 8.5, 7, 5, 7, 7, 5, 8, 7, 8, 7, 6],
+        Firefly: [10, 10, 7, 8, 7.5, 6, 10, 10, 6.66, 8, 8, 10, 9, 9, 6, 8],
+        'League of Downwire': [7.5,5,5,6,6,8,7,7,6,6,7.77,7,5,5],
+        'Low Level Zone': [8,6,7.5,7,6,6,9,9.89,9,4,7,5,7,8,9],
+        'Reapers Squad': [6,9,8,6,7,8,10,10,7,10,8,6,9.99,10,7],
+        'Red Light': [9,7,9.99,10,10,4,8,8,7,6,5,9,5,6,6,5],
+        'Show Me The Way': [10,10,5,9,4,5,6,8,8,8,7,10,10,10,10,8],
+        'The Chase': [7,10,10,8.5,6,5,8.8,6,6,5,7,5,7,5,7,8],
+        'This Is Not The End': [6,10,8,9,8,9,7,5,9,9.2,6,8,7.77,7,10]
+    }
+
+    const [offset, setOffset] = useState(-1)
+    const {data, refetch} = useGetData(offset);
     const [commentsData, setCommentsData] = useState({});
-    const songListInitialOrder = Object.keys(commentsData || {})
+    const songListInitialOrder = Object.keys(MOCK_RATING || {})
     const [sortType, setSortType] = useState('songNumber');
     const [songList, setSongList] = useState([])
 
+    // useEffect(() => {
+    //     setCommentsData((prevState => (
+    //         {...data?.result?.reduce((acc, updateData) => {
+    //
+    //                 const message = updateData?.edited_message || updateData?.message
+    //
+    //                 if (!!message?.reply_to_message) {
+    //                     const songName = getSongNameFromText(message?.reply_to_message?.text || message?.reply_to_message?.caption);
+    //                     const senderName = message?.from?.first_name || message?.from?.username || '';
+    //                     let rating = message?.text?.replaceAll(',', '.').replace(/[^0-9.]/g, '');
+    //
+    //                     if (!songName) {
+    //                         return acc;
+    //                     }
+    //
+    //                     if (!Number.isNaN(rating)) {
+    //                         const ratingFormatted = Number(rating);
+    //                         if (ratingFormatted > 10) {
+    //                             rating = 10;
+    //                         } else if (ratingFormatted < 0) {
+    //                             rating = 0;
+    //                         } else {
+    //                             rating = ratingFormatted;
+    //                         }
+    //                     } else {
+    //                         return acc;
+    //                     }
+    //
+    //                     return {
+    //                         ...acc, [songName]: {
+    //                             ...acc?.[songName], [senderName]: rating,
+    //                         }
+    //                     }
+    //                 }
+    //
+    //                 return acc;
+    //             }, {...prevState})}
+    //     )))
+    //
+    //     if (data?.result?.length >= 100) {
+    //         setOffset(data?.result?.[99]?.update_id + 1)
+    //     }
+    // }, [data])
+
     useEffect(() => {
-        setCommentsData((prevState => (
-            {...prevState, ...data?.result?.reduce((acc, updateData) => {
 
-                    const message = updateData?.edited_message || updateData?.message
-
-                    if (!!message?.reply_to_message) {
-                        const songName = getSongNameFromText(message?.reply_to_message?.text || message?.reply_to_message?.caption);
-                        const senderName = message?.from?.first_name || message?.from?.username || '';
-                        let rating = message?.text?.replaceAll(',', '.').replace(/[^0-9.]/g, '');
-
-                        if (!songName) {
-                            return acc;
-                        }
-
-                        if (!Number.isNaN(rating)) {
-                            const ratingFormatted = Number(rating);
-                            if (ratingFormatted > 10) {
-                                rating = 10;
-                            } else if (ratingFormatted < 0) {
-                                rating = 0;
-                            } else {
-                                rating = ratingFormatted;
-                            }
-                        } else {
-                            return acc;
-                        }
-
-                        return {
-                            ...acc, [songName]: {
-                                ...acc?.[songName], [senderName]: rating,
-                            }
-                        }
-                    }
-
-                    return acc;
-                }, {})}
-        )))
-
-        if (data?.result?.length >= 100) {
-            setOffset(data?.result?.[99]?.update_id + 1)
-        }
-    }, [data])
-
-    useEffect(() => {
-        if (!commentsData) {
-            return;
-        }
-
-        const newSongList = Object.keys(commentsData)
+        const newSongList = Object.keys(MOCK_RATING)
 
         if (sortType === 'rating') {
             setSongList(newSongList.sort((songNameA, songNameB) => {
-                const a = Object.values(commentsData[songNameA]).reduce((acc, rate) => acc + rate, 0);
-                const b = Object.values(commentsData[songNameB]).reduce((acc, rate) => acc + rate, 0);
+                const a = Object.values(MOCK_RATING[songNameA]).reduce((acc, rate) => acc + rate, 0);
+                const b = Object.values(MOCK_RATING[songNameB]).reduce((acc, rate) => acc + rate, 0);
 
                 return b - a;
             }))
@@ -100,11 +110,29 @@ function App() {
             setSongList(newSongList);
         }
 
+        // if (!commentsData) {
+        //     return;
+        // } else {
+        //     const newSongList = Object.keys(commentsData)
+        //
+        //     if (sortType === 'rating') {
+        //         setSongList(newSongList.sort((songNameA, songNameB) => {
+        //             const a = Object.values(commentsData[songNameA]).reduce((acc, rate) => acc + rate, 0);
+        //             const b = Object.values(commentsData[songNameB]).reduce((acc, rate) => acc + rate, 0);
+        //
+        //             return b - a;
+        //         }))
+        //     } else {
+        //         setSongList(newSongList);
+        //     }
+        // }
+
     }, [commentsData, sortType])
 
     return (
         <>
-            <div className={'background'} style={{backgroundImage: `${import.meta.env.BASE_URL}/image_2024-10-24_22-40-36.png`}}/>
+            <div className={'background'}
+                 style={{backgroundImage: `${import.meta.env.BASE_URL}/image_2024-10-24_22-40-36.png`}}/>
             <div className={'root'}>
                 <div className={'songListHeader'}>
                     <div style={{padding: '0 10px', cursor: 'pointer'}} onClick={() => {
@@ -120,8 +148,8 @@ function App() {
                 <FlipMove className={'songList'}>
                     {songList.map((songName) => {
                         const songPosition = songListInitialOrder.findIndex((songNameFromList => songNameFromList === songName)) + 1
-                        const ratingList = Object.keys(commentsData[songName])
-                        const averageRating = ratingList.reduce((acc, senderName) => acc + commentsData[songName][senderName], 0) / ratingList.length
+                        const ratingList = MOCK_RATING[songName]
+                        const averageRating = ratingList.reduce((acc, rating) => acc + Number(rating), 0) / ratingList.length
                         const ratingRound = Math.round((averageRating + Number.EPSILON) * 100) / 100
 
                         return (
